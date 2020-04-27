995b2a48518338d9d61cfc5721f299828b47819e
Leonel Rosales <rleonel426@gmail.com>
2020-04-26 17:41:14 -0700

    uploading files

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,13 @@ module.exports = {
         filename: 'bundle.js',
         path: path.join(__dirname, 'public')
     },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
