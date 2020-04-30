5cfe8453f5569c3b591db6cd1054db1466b0d40e
Leonel Rosales <rleonel426@gmail.com>
2020-04-29 19:17:19 -0700

    adding back and front end

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //import express
 const express = require('express');
+// importing axios
+const axios = require('axios');
 //we call the app
 const app = express();
 
@@ -7,6 +9,15 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
+
+app.get('/users', (req, res) => {
+    //we are getting the API url
+    axios.get('https://randomuser.me/api/?page=1&results=10')
+        .then(response => {
+            //data is what we want from the url
+            res.send(response.data);
+        });
+});
 
 //where we want our applciation to listen port
 app.listen(3000, () => {
